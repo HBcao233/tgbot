96c03c14fd265c6cce5e9c8fb9528d2d596a8888
HBcao233 <hbcaoqaq@gmail.com>
2024-07-25 05:19:38 +0800

    feat: 增加机器人的代理认证信息配置 #1

--- config.py
+++ config.py
@@ -19,8 +19,8 @@ class Config:
     self.base_file_url = self.env.get('base_file_url', 'https://api.telegram.org/file/bot')
     local_mode = self.env.get('local_mode', False)
     self.local_mode = True if local_mode in ['1', 'true', 'True', 'T'] else False
-      
-    self.echo_chat_id = int(self.env.get('echo_chat_id', 0))
+    
+    self.echo_chat_id = int(e) if (e := self.env.get('echo_chat_id', '')) else 0
     self.superadmin = [int(x) for x in self.env.get('superadmin', '').split(',') if x]
     
     self.telegraph_author_name = self.env.get('telegraph_author_name', '')
@@ -34,6 +34,12 @@ class Config:
       if self.env.get('proxy_host', '') != '':
         host = self.env.get('proxy_host', '')
       self.proxy_url = f'http://{host}:{port}/'
+    
+      proxy_user = self.env.get('proxy_user', '')
+      proxy_pass = self.env.get('proxy_pass', '')
+      if proxy_user and proxy_pass:
+          self.proxy_url = f'http://{proxy_user}:{proxy_pass}@{host}:{port}'
+          
     if self.proxy_url is not None:
       self.proxies.update({
         "http://": self.proxy_url,
